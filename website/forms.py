298791f2ddcdbdc3af0581d8b787bfb00d28a805
abdallah <abdallahmohamed5528672@gmail.com>
2023-03-05 11:05:55 +0200

    registration

--- website/forms.py
+++ website/forms.py
@@ -2,7 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
-class SignUpFor(UserCreationForm):
+class SignUpForm(UserCreationForm):
     email = forms.EmailField(label="",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email Address'}))
     first_name = forms.CharField(label="",max_length="50",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}))
     last_name = forms.CharField(label="",max_length="50",widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}))
@@ -11,7 +11,7 @@ class SignUpFor(UserCreationForm):
         model = User
         fields = ('username','first_name','last_name','email','password1','password2')
     def __init__(self, *args,**kwargs):
-        super(SignUpFor,self).__init__(*args,**kwargs)
+        super(SignUpForm,self).__init__(*args,**kwargs)
 
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['username'].widget.attrs['placeholder'] = 'User Name'
@@ -27,4 +27,3 @@ class SignUpFor(UserCreationForm):
         self.fields['password2'].widget.attrs['placeholder'] = 'confirm password'
         self.fields['password2'].label=''
         self.fields['password2'].help_text ='span class="form-text text-muted"><small>Enter the same word as befor,for verification.</small><span>'
-        
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 # Create your views here.
 
